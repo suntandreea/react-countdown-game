3af2c3c0e85e454c31590b023203a18644e26e65
Andreea <suntandreea@protonmail.com>
2024-05-29 21:11:11 +0300

    portals

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,4 +1,5 @@
 import {forwardRef, useImperativeHandle, useRef} from 'react';
+import {createPortal} from 'react-dom';
 
 const ResultModal = forwardRef(function ({targetTime, remainingTime, handleReset}, ref) {
   const innerDialogRef = useRef();
@@ -15,14 +16,17 @@ const ResultModal = forwardRef(function ({targetTime, remainingTime, handleReset
     };
   });
 
-  return <dialog ref={innerDialogRef} className="result-modal" onClose={handleReset}>
-    {userLost ? <h2>You lost!</h2> : <h2>Your score: {score}</h2>}
-    <p>The target time was <strong>{targetTime} seconds.</strong></p>
-    <p>You stopped the tier with <strong>{formattedTime} seconds left.</strong></p>
-    <form method="dialog" onSubmit={handleReset}>
-      <button>Close</button>
-    </form>
-  </dialog>
+  return createPortal(
+    <dialog ref={innerDialogRef} className="result-modal" onClose={handleReset}>
+      {userLost ? <h2>You lost!</h2> : <h2>Your score: {score}</h2>}
+      <p>The target time was <strong>{targetTime} seconds.</strong></p>
+      <p>You stopped the tier with <strong>{formattedTime} seconds left.</strong></p>
+      <form method="dialog" onSubmit={handleReset}>
+        <button>Close</button>
+      </form>
+    </dialog>,
+    document.getElementById('modal')
+  );
 })
 
 export default ResultModal;
