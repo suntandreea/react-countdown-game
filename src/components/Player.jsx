ea4ba20205a307664a83686ec6c068b0c62a3234
Andreea <suntandreea@protonmail.com>
2024-05-29 19:30:11 +0300

    forwardRef

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,20 @@
+import {useRef, useState} from 'react';
+
 export default function Player() {
+  const [name, setName] = useState(null);
+  const nameInput = useRef();
+
+  function handleNameChange() {
+    setName(nameInput.current.value);
+    nameInput.current.value = '';
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {name ?? 'unknown entity'}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" ref={nameInput} />
+        <button onClick={handleNameChange}>Set Name</button>
       </p>
     </section>
   );
---
+++ src/components/TimerChallenge.jsx
@@ -0,0 +1,43 @@
+import {useRef, useState} from 'react';
+import ResultModal from './ResultModal.jsx';
+
+export default function TimerChallenge({title, targetTime}) {
+  const timer = useRef();
+  const dialog= useRef();
+  const [hasStarted, setHasStarted] = useState(false);
+  const [isExpired, setIsExpired] = useState(false);
+
+  function handleStart() {
+    setHasStarted(true);
+    timer.current = setTimeout(() => {
+      setIsExpired(true);
+      setHasStarted(false);
+      dialog.current.showModal();
+    }, targetTime * 1000);
+  }
+
+  function handleStop() {
+    setHasStarted(false);
+    clearTimeout(timer.current);
+  }
+
+  return <>
+    <ResultModal ref={dialog} targetTime={targetTime} result='lost' />
+    <section className="challenge">
+      <h2>{title}</h2>
+      <p className="challenge-time">
+        {targetTime + ' second' + (targetTime > 1 ? 's' : '')}
+      </p>
+      <p>
+        {hasStarted
+          ? <button onClick={handleStop}>Stop Challenge</button>
+          : <button onClick={handleStart}>Start Challenge</button>
+        }
+      </p>
+      {hasStarted
+        ? <p className="active">Time is running...</p>
+        : <p>Timer inactive.</p>
+      }
+    </section>
+  </>
+}
